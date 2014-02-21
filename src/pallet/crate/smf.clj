2c8d231558a551c6d76ee90b2827bf475613e92d
Adam Tait <adam@adamtait.com>
2014-02-20 18:40:07 -0800

    fix all the problems! sadly, these changes were actually multiple commits, but have been compressed into one here.

--- src/pallet/crate/smf.clj
+++ src/pallet/crate/smf.clj
@@ -9,7 +9,7 @@
                  "http://www.oracle.com/technetwork/server-storage/solaris/solaris-smf-manifest-wp-167902.pdf"
                  "Manifold Examle" "http://chrismiles.livejournal.com/26279.html" ]
       }
-  pallet.crate.smf
+  eReceipts.pallet.crates.smf
   (:require [clojure.data.xml :as xml]
             [clojure.java.io :as io]
             [clojure.tools.logging :refer [debugf]]
@@ -73,7 +73,7 @@
   (with-open [r (io/reader fname)]
     (doall (line-seq r))))
 
-(defn- smf-defaults
+(def smf-defaults
   {:multiple-instances? false
    :instance-name "default"
    :config-file ""
@@ -86,7 +86,7 @@
    :working-dir nil})
 
 
-(defn create-smf
+(defn create-manifest-xml
   "This will create smf clojure data that can be exported to xml.  Please note
    this does not include the DOCTYPE header for the xml file.  The DOCTYPE gets
    output in the write function.
@@ -114,7 +114,6 @@
            {:keys [multiple-instances?
                    instance-name
                    config-file
-                   start-command
                    stop-command
                    process-management
                    network?
@@ -122,6 +121,7 @@
                    timeout
                    stability-value
                    working-dir]} merged]
+
        [:service_bundle {:type "manifest" :name service-name}
         [:service {:name (str service-category "/" service-name)
                    :type "service"
@@ -136,32 +136,36 @@
          (create-property-group-startd (name process-management))
          (create-property-group-application)
          [:stability {:value (name stability-value)}]]]))
-  ([service-category service-name service-version start-command user group]
-     (create-smf service-category service-name service-version start-command
-                 user group {})))
 
-(defn write-smf
-  "Write out the valid smf-data clojure file to a temporary file as xml
-   return the name of the temporary file.  If delete-temp-file is true then
-   the temp file is deleted on exit of the application per Java guidelines
-   regarding deleteOnExit"
-  [smf-data & [delete-temp-file?]]
-  (let [temp (java.io.File/createTempFile "pallet" "smf")
-        smf-xml (xml/indent-str (xml/sexp-as-element smf-data))
+  ([service-category
+    service-name
+    service-version
+    start-command
+    user
+    group]
+     (create-manifest-xml service-category
+                          service-name
+                          service-version
+                          start-command
+                          user
+                          group
+                          {})))
+
+(defn- get-service-manifest-content
+  "build SMF manifest XML file content"
+  [smf-data
+   & [delete-temp-file?]]
+  (let [smf-xml (xml/indent-str (xml/sexp-as-element smf-data))
         ;; Cannot easily add a doctype so have to process the header.
         ;; Insert the DOCTYPE, and then insert the rest
         lines (clojure.string/split-lines smf-xml)
         first-line (first lines)
-        rest-lines (subvec lines 1)
-        output [ (str first-line "\n") "<!DOCTYPE service_bundle SYSTEM \"/usr/share/lib/xml/dtd/service_bundle.dtd.1\">\n"
-                 (clojure.string/join "\n" rest-lines)]
-
-        ]
-    (if delete-temp-file? (.deleteOnExit temp))
-    (with-open [wrtr (io/writer temp)]
-      (doseq [line output]
-        (.write wrtr line)))
-    (.getAbsolutePath temp)))
+        rest-lines (subvec lines 1)]
+    (clojure.string/join "\n"
+                         (apply vector
+                                (str first-line "\n")
+                                "<!DOCTYPE service_bundle SYSTEM \"/usr/share/lib/xml/dtd/service_bundle.dtd.1\">"
+                                rest-lines))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ## Service Supervisor
@@ -184,22 +188,31 @@
          (not (and manifest-data init-file))]}
   (debugf "Adding service settings for %s" service-name)
   (assoc-in-settings [:smf :method-dir] "/opt/custom/bin")
+  (assoc-in-settings [:smf :manifest-dir] "/opt/custom/smf")
   (assoc-in-settings [:smf :services service-name]
                      (merge (default-service-options)
                             service-options)))
 
-(defn- svcadm
-  [service-name action]
-  {:pre [service-name action]}
-  (exec-checked-script
-   (str "SMF " (name action) " " service-name)
-   ("/usr/sbin/svcadm" ~(name action) ~service-name)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ## Pallet service-supervisor actions
 
 (def ^:private init->smf-action
   {:start   :enable
    :stop    :disable
    :enable  :enable
    :restart :restart})
+
+(defn- perform-service-action
+  [service-name
+   action]
+  {:pre [(string? service-name)
+         (keyword? action)]}
+  (let [action-name (name action)]
+    (exec-checked-script
+     (str "running SMF service action " action-name " on " service-name)
+     ("svcadm" ~action-name ~service-name))))
 
 (defmethod service/service-supervisor :smf
   [_
@@ -208,35 +221,41 @@
     :or {action :start}
     :as options}]
   {:pre [service-name action]}
-  (println "action:" action)
   (assert (not (contains? options :if-stopped))
           "SMF does not support the concept of if-stopped")
   (let [smf-action (init->smf-action action)]
     (if if-flag
       (plan-when (crate/target-flag? if-flag)
-        (svcadm service-name smf-action))
-      (svcadm service-name smf-action))))
+        (perform-service-action service-name smf-action))
+      (perform-service-action service-name smf-action))))
 
-(defn- service-configured?
-  [service-name]
-  (stevedore/fragment ("/usr/bin/svcs" ~service-name "2&>" "/dev/null")))
 
 (defn install-smf-service
-  [smf-data remote-path & [delete-temp-file?]]
+  "install the new SMF manifest"
+  [smf-data
+   remote-path]
   (remote-file remote-path
-               :local-file (write-smf smf-data delete-temp-file?))
+               :literal true
+               :content (get-service-manifest-content smf-data true))
   (exec-checked-script
-   (str "install SMF service " remote-path)
-   ("/usr/sbin/svccfg validate " ~remote-path)
-   ("/usr/sbin/svccfg import " ~remote-path)))
+   (str "install smf service")
+   ("svccfg validate " ~remote-path)
+   ("svccfg import " ~remote-path)))
+
 
 (defn delete-smf-service
-  "Delete the specified smf service.  If force is true it will force delete the service"
+  "Delete the specified smf service.
+    'force' will force delete"
   [smf-name force?]
-  (let [command (if force? "svccfg delete -f " "svccfg delete ")]
+  (let [command (if force?
+                  "svccfg delete -f "
+                  "svccfg delete ")]
     (exec-checked-script
-     (str "Delete the service " smf-name)
      (~command ~smf-name))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Install Service for init-file or smf-data
 
 (defn get-init-file-path
   [settings service-name]
@@ -247,19 +266,22 @@
   (fn [settings service-name service-options]
     (if (:init-file service-options)
       :init-file
-      :smf-data)))
+      :manifest-xml)))
 
 (defmethod install-service :init-file
-  [settings service-name service-options]
+  [settings
+   service-name
+   service-options]
   (let [init-file-path (get-init-file-path settings service-name)
-        smf-data (create-smf (:service-category service-options)
-                             service-name
-                             (:version service-options)
-                             (str init-file-path " start")
-                             (:user service-options)
-                             (:group service-options)
-                             (:options service-options))]
-
+        start-command (str init-file-path " start")
+        manifest-xml (create-manifest-xml (:service-category service-options)
+                                          service-name
+                                          (:version service-options)
+                                          start-command
+                                          (:user service-options)
+                                          (:group service-options)
+                                          (:options service-options))
+        manifest-path (str (:manifest-dir settings) "/" service-name "-manifest.xml")]
     (apply-map remote-file
                init-file-path
                :mode "0755"
@@ -267,29 +289,43 @@
                :group "root"
                :literal true
                (:init-file service-options))
-    (install-smf-service smf-data
-                         (str "/tmp/" service-name "-manifest.xml"))))
 
-(defmethod install-service :smf-data
+    (install-smf-service manifest-xml
+                         manifest-path)))
+
+(defmethod install-service :manifest-xml
   [service-name service-options]
   (install-smf-service (:manifest-data service-options)
                        (:manifest-path service-options)))
 
-(defn configure
-  "Install the manifest file for each configured service so that
-  services can be managed by smf"
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ## pallet configure phase
+
+(crate/defplan configure-plan
+  "create a new SMF manifest and load it"
   [{:keys [instance-id] :as options}]
 
-  (let [settings (crate/get-settings :smf {:instance-id instance-id})
+  (let [settings (crate/get-settings :smf
+                                     {:instance-id instance-id})
         services (:services settings)]
     (directory (:method-dir settings))
-    (println "got my services" services)
+    (directory (:manifest-dir settings))
+
+    (println "services settings /")
+    (clojure.pprint/pprint services)
+    (println "/")
+
     (doseq [[service-name service-options] services]
-      (install-service settings service-name service-options)
-      #_(plan-when-not (service-configured? service-name)
-        (install-service settings service-name service-options)))))
+      (install-service settings
+                       service-name
+                       service-options))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ## Pallet Server Specification
 
 (defn server-spec
   [& {:keys [instance-id] :as options}]
+  ;; TODO move this from :configure to :bootstrap
   (api/server-spec
-   :phases {:configure (api/plan-fn (configure options))}))
+   :phases {:configure (api/plan-fn (configure-plan options))}))
